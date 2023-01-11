a045f63462133a8b869a28694364e9e8b9888e84
Amric Trudel <amrictrudel.42@gmail.com>
2023-01-11 19:10:47 +0100

    fix output path

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ from decouple import config
 
 
 DATA_DIR = config('DATA_DIR', default='/kaggle/input/proteinchallenge')
-OUTPUT_DIR = config('OUTPUT_DIR', default='kaggle/working')
+OUTPUT_DIR = config('OUTPUT_DIR', default='/kaggle/working')
