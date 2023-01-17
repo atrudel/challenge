44f488e065aeb4dca85a894636f5128bf9c7833a
Amric Trudel <amrictrudel.42@gmail.com>
2023-01-17 12:46:26 +0100

    implement hyperparameter search with raytune

--- training/model_training.py
+++ training/model_training.py
@@ -16,9 +16,8 @@ from ray.air import session
 from graph.models.baseline import GNN
 
 
-def train(epoch, model, loss_function, optimizer, features_train, y_train, hparams):
+def train(epoch, model, loss_function, optimizer, features_train, y_train, hparams, search=False):
     adj_train, node_features_train, edge_features_train = features_train
-    N_train = len(y_train)
     batch_size = hparams['batch_size']
 
     model.train()
@@ -27,30 +26,12 @@ def train(epoch, model, loss_function, optimizer, features_train, y_train, hpara
     count = 0
 
     # Iterate over the batches
-    with tqdm(range(0, N_train, batch_size), unit='batch') as tepoch:
+    with tqdm(range(0, len(y_train), batch_size), unit='batch', disable=search) as tepoch:
         tepoch.set_description(f"Epoch {epoch}")
         for i_batch in tepoch:
-            adj_batch = list()
-            features_batch = list()
-            idx_batch = list()
-            y_batch = list()
-
-            # Create tensors
-            for j in range(i_batch, min(N_train, i_batch + batch_size)):
-                n = adj_train[j].shape[0]
-                adj_batch.append(adj_train[j] + sp.identity(n))
-                features_batch.append(node_features_train[j])
-                idx_batch.extend([j - i_batch] * n)
-                y_batch.append(y_train[j])
-
-            adj_batch = sp.block_diag(adj_batch)
-            features_batch = np.vstack(features_batch)
-
-            adj_batch = sparse_mx_to_torch_sparse_tensor(adj_batch).to(device)
-            features_batch = torch.FloatTensor(features_batch).to(device)
-            idx_batch = torch.LongTensor(idx_batch).to(device)
-            y_batch = torch.LongTensor(y_batch).to(device)
-
+            adj_batch, features_batch, idx_batch, y_batch = compute_batch_features(
+                adj_train, node_features_train, y_train, batch_size, i_batch
+            )
             # Step
             optimizer.zero_grad()
             output = model(features_batch, adj_batch, idx_batch)
@@ -66,11 +47,12 @@ def train(epoch, model, loss_function, optimizer, features_train, y_train, hpara
             tepoch.set_postfix(loss_train=loss_train, acc_train=acc_train)
 
         loss_train = train_loss / count
-        return loss_train
+        return loss_train, acc_train
 
-def validate(epoch, model, loss_function, features_val, y_val, hparams):
+
+
+def validate(epoch, model, loss_function, features_val, y_val, hparams, search=False):
     adj_val, node_features_val, edge_features_val = features_val
-    N_val = len(y_val)
     batch_size = hparams['batch_size']
     val_loss = 0
     correct = 0
@@ -78,27 +60,10 @@ def validate(epoch, model, loss_function, features_val, y_val, hparams):
 
     model.eval()
     with torch.no_grad():
-        for i_batch in range(0, N_val, batch_size):
-            adj_batch = list()
-            features_batch = list()
-            idx_batch = list()
-            y_batch = list()
-
-            # Create tensors
-            for j in range(i_batch, min(N_val, i_batch + batch_size)):
-                n = adj_val[j].shape[0]
-                adj_batch.append(adj_val[j] + sp.identity(n))
-                features_batch.append(node_features_val[j])
-                idx_batch.extend([j - i_batch] * n)
-                y_batch.append(y_val[j])
-
-            adj_batch = sp.block_diag(adj_batch)
-            features_batch = np.vstack(features_batch)
-
-            adj_batch = sparse_mx_to_torch_sparse_tensor(adj_batch).to(device)
-            features_batch = torch.FloatTensor(features_batch).to(device)
-            idx_batch = torch.LongTensor(idx_batch).to(device)
-            y_batch = torch.LongTensor(y_batch).to(device)
+        for i_batch in range(0, len(y_val), batch_size):
+            adj_batch, features_batch, idx_batch, y_batch = compute_batch_features(
+                adj_val, node_features_val, y_val, batch_size, i_batch
+            )
 
             output = model(features_batch, adj_batch, idx_batch)
             loss = loss_function(output, y_batch)
@@ -109,17 +74,45 @@ def validate(epoch, model, loss_function, features_val, y_val, hparams):
 
         loss_val = val_loss / count
         acc_val = correct / count
-        print('Epoch: {:03d}'.format(epoch),
-              'loss_val: {:.4f}'.format(loss_val),
-              'acc_val: {:.4f}'.format(acc_val),
-             )
-    return loss_val
+        if not search:
+            print('Epoch: {:03d}'.format(epoch),
+                  'loss_val: {:.4f}'.format(loss_val),
+                  'acc_val: {:.4f}'.format(acc_val),
+                 )
+    return loss_val, acc_val
 
-def run(hparams, experiment_name=None, seed=42):
-    if experiment_name is None:
-        experiment_name = "experiment"
-    experiment_name += f"_{datetime.now(tz=pytz.timezone('Europe/Paris')).strftime('%Y-%m-%d_%Hh%Mm%Ss')}"
-    print(f"Launching new experiment {experiment_name}")
+
+def compute_batch_features(adj, node_features, y, batch_size, i_batch):
+    N_train = len(y)
+    adj_batch = list()
+    features_batch = list()
+    idx_batch = list()
+    y_batch = list()
+
+    # Create tensors
+    for j in range(i_batch, min(N_train, i_batch + batch_size)):
+        n = adj[j].shape[0]
+        adj_batch.append(adj[j] + sp.identity(n))
+        features_batch.append(node_features[j])
+        idx_batch.extend([j - i_batch] * n)
+        y_batch.append(y[j])
+
+    adj_batch = sp.block_diag(adj_batch)
+    features_batch = np.vstack(features_batch)
+    adj_batch = sparse_mx_to_torch_sparse_tensor(adj_batch).to(device)
+    features_batch = torch.FloatTensor(features_batch).to(device)
+    idx_batch = torch.LongTensor(idx_batch).to(device)
+    y_batch = torch.LongTensor(y_batch).to(device)
+    return adj_batch, features_batch, idx_batch, y_batch
+
+
+
+def run(hparams, experiment_name=None, seed=42, search=True):
+    if not search:
+        if experiment_name is None:
+            experiment_name = "experiment"
+        experiment_name += f"_{datetime.now(tz=pytz.timezone('Europe/Paris')).strftime('%Y-%m-%d_%Hh%Mm%Ss')}"
+        print(f"Launching new experiment {experiment_name}")
 
     # Data Loading
     adj, node_features, edge_features = load_graph_data()
@@ -140,15 +133,17 @@ def run(hparams, experiment_name=None, seed=42):
     # Training loop
     best_val_loss = float('inf')
     for epoch in range(hparams['epochs']):
-        train_loss = train(epoch, model, loss_function, optimizer, features_train, y_train, hparams)
-        val_loss = validate(epoch, model, loss_function, features_val, y_val, hparams)
+        train_loss, train_acc = train(epoch, model, loss_function, optimizer, features_train, y_train, hparams, search)
+        val_loss, val_acc = validate(epoch, model, loss_function, features_val, y_val, hparams, search)
         session.report({
             "train_loss": train_loss,
-            "val_loss": val_loss
+            "train_acc": train_acc,
+            "val_loss": val_loss,
+            "val_acc": val_acc
         })
 
-        # Save checkpoint if val loss improved
-        if val_loss < best_val_loss:
+        # Save checkpoint if val loss improved and if not in hyperaparamter search
+        if search is False and val_loss < best_val_loss:
             save_dir = f"{CHECKPOINT_DIR}/{experiment_name}"
             os.makedirs(save_dir, exist_ok=True)
             save_path = f"{save_dir}/model_epoch={epoch}_val-loss={val_loss:.3f}.pth"
@@ -168,6 +163,7 @@ def run(hparams, experiment_name=None, seed=42):
 
 
 if __name__ == '__main__':
+    # Example
     hparams = {
         'epochs': 10,
         'batch_size': 64,
