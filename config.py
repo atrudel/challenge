839d33a0fcf82b8c489f8733b1ae02729cfc1165
Amric Trudel <amrictrudel.42@gmail.com>
2023-01-17 11:47:20 +0100

    refactored training code

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from decouple import config
 from datatest import working_directory
 import os
 from pathlib import Path
+import torch
 
 with working_directory(__file__):
     current_dir = os.path.abspath('.')
@@ -14,11 +15,16 @@ if KAGGLE:
     DATA_DIR = '/kaggle/input/proteinchallenge'
     OUTPUT_DIR = '/kaggle/working'
     CACHE_DIR = '/kaggle/working/cache'
+    CHECKPOINT_DIR = '/kaggle/working/checkpoints'
 
 # In a local environment, config variables can be overriden with a .env file at the root of the repository
 else:
     DATA_DIR = config('DATA_DIR', default=ROOT_DIR/'data')
     OUTPUT_DIR = config('OUTPUT_DIR', default=ROOT_DIR/'output')
     CACHE_DIR = config('CACHE_DIR', default=ROOT_DIR/'cache')
+    CHECKPOINT_DIR = config('CHECKPOINT_DIR', default=ROOT_DIR/'checkpoints')
 
 os.makedirs(CACHE_DIR, exist_ok=True)
+os.makedirs(CHECKPOINT_DIR, exist_ok=True)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
---
+++ training/model_training.py
@@ -0,0 +1,180 @@
+import os
+from datetime import datetime
+
+import numpy as np
+import pytz
+from tqdm import tqdm
+import scipy.sparse as sp
+from torch import optim
+
+from graph.data_processing import load_graph_data, normalize_adjacency, sparse_mx_to_torch_sparse_tensor, train_test_split
+import torch
+import torch.nn as nn
+from config import device, CHECKPOINT_DIR
+from ray.air import session
+
+from graph.models.baseline import GNN
+
+
+def train(epoch, model, loss_function, optimizer, features_train, y_train, hparams):
+    adj_train, node_features_train, edge_features_train = features_train
+    N_train = len(y_train)
+    batch_size = hparams['batch_size']
+
+    model.train()
+    train_loss = 0
+    correct = 0
+    count = 0
+
+    # Iterate over the batches
+    with tqdm(range(0, N_train, batch_size), unit='batch') as tepoch:
+        tepoch.set_description(f"Epoch {epoch}")
+        for i_batch in tepoch:
+            adj_batch = list()
+            features_batch = list()
+            idx_batch = list()
+            y_batch = list()
+
+            # Create tensors
+            for j in range(i_batch, min(N_train, i_batch + batch_size)):
+                n = adj_train[j].shape[0]
+                adj_batch.append(adj_train[j] + sp.identity(n))
+                features_batch.append(node_features_train[j])
+                idx_batch.extend([j - i_batch] * n)
+                y_batch.append(y_train[j])
+
+            adj_batch = sp.block_diag(adj_batch)
+            features_batch = np.vstack(features_batch)
+
+            adj_batch = sparse_mx_to_torch_sparse_tensor(adj_batch).to(device)
+            features_batch = torch.FloatTensor(features_batch).to(device)
+            idx_batch = torch.LongTensor(idx_batch).to(device)
+            y_batch = torch.LongTensor(y_batch).to(device)
+
+            # Step
+            optimizer.zero_grad()
+            output = model(features_batch, adj_batch, idx_batch)
+            loss = loss_function(output, y_batch)
+            train_loss += loss.item() * output.size(0)
+            count += output.size(0)
+            preds = output.max(1)[1].type_as(y_batch)
+            correct += torch.sum(preds.eq(y_batch).double()).item()
+            loss.backward()
+            optimizer.step()
+            loss_train = train_loss / count
+            acc_train = correct / count
+            tepoch.set_postfix(loss_train=loss_train, acc_train=acc_train)
+
+        loss_train = train_loss / count
+        return loss_train
+
+def validate(epoch, model, loss_function, features_val, y_val, hparams):
+    adj_val, node_features_val, edge_features_val = features_val
+    N_val = len(y_val)
+    batch_size = hparams['batch_size']
+    val_loss = 0
+    correct = 0
+    count = 0
+
+    model.eval()
+    with torch.no_grad():
+        for i_batch in range(0, N_val, batch_size):
+            adj_batch = list()
+            features_batch = list()
+            idx_batch = list()
+            y_batch = list()
+
+            # Create tensors
+            for j in range(i_batch, min(N_val, i_batch + batch_size)):
+                n = adj_val[j].shape[0]
+                adj_batch.append(adj_val[j] + sp.identity(n))
+                features_batch.append(node_features_val[j])
+                idx_batch.extend([j - i_batch] * n)
+                y_batch.append(y_val[j])
+
+            adj_batch = sp.block_diag(adj_batch)
+            features_batch = np.vstack(features_batch)
+
+            adj_batch = sparse_mx_to_torch_sparse_tensor(adj_batch).to(device)
+            features_batch = torch.FloatTensor(features_batch).to(device)
+            idx_batch = torch.LongTensor(idx_batch).to(device)
+            y_batch = torch.LongTensor(y_batch).to(device)
+
+            output = model(features_batch, adj_batch, idx_batch)
+            loss = loss_function(output, y_batch)
+            val_loss += loss.item() * output.size(0)
+            count += output.size(0)
+            preds = output.max(1)[1].type_as(y_batch)
+            correct += torch.sum(preds.eq(y_batch).double()).item()
+
+        loss_val = val_loss / count
+        acc_val = correct / count
+        print('Epoch: {:03d}'.format(epoch),
+              'loss_val: {:.4f}'.format(loss_val),
+              'acc_val: {:.4f}'.format(acc_val),
+             )
+    return loss_val
+
+def run(hparams, experiment_name=None, seed=42):
+    if experiment_name is None:
+        experiment_name = "experiment"
+    experiment_name += f"_{datetime.now(tz=pytz.timezone('Europe/Paris')).strftime('%Y-%m-%d_%Hh%Mm%Ss')}"
+    print(f"Launching new experiment {experiment_name}")
+
+    # Data Loading
+    adj, node_features, edge_features = load_graph_data()
+    adj = [normalize_adjacency(A) for A in adj]
+    features_train, y_train, features_val, y_val, features_test, proteins_test = train_test_split(
+        adj,
+        node_features,
+        edge_features,
+        val_size=0.25,
+        random_state=seed
+    )
+
+    # Model initialization
+    model = GNN(hparams['n_input'], hparams['n_hidden'], hparams['dropout'], 18).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=hparams['learning_rate'])
+    loss_function = nn.CrossEntropyLoss()
+
+    # Training loop
+    best_val_loss = float('inf')
+    for epoch in range(hparams['epochs']):
+        train_loss = train(epoch, model, loss_function, optimizer, features_train, y_train, hparams)
+        val_loss = validate(epoch, model, loss_function, features_val, y_val, hparams)
+        session.report({
+            "train_loss": train_loss,
+            "val_loss": val_loss
+        })
+
+        # Save checkpoint if val loss improved
+        if val_loss < best_val_loss:
+            save_dir = f"{CHECKPOINT_DIR}/{experiment_name}"
+            os.makedirs(save_dir, exist_ok=True)
+            save_path = f"{save_dir}/model_epoch={epoch}_val-loss={val_loss:.3f}.pth"
+            print(f"Validation loss improved, saving checkpoint to {save_path}")
+            print()
+            checkpoint = {
+                'experiment_name': experiment_name,
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss/train': train_loss,
+                'loss/val': val_loss,
+                'hparams': hparams
+            }
+            torch.save(checkpoint, save_path)
+    return best_val_loss
+
+
+if __name__ == '__main__':
+    hparams = {
+        'epochs': 10,
+        'batch_size': 64,
+        'n_hidden': 64,
+        'n_input': 86,
+        'dropout': 0.2,
+        'learning_rate': 0.001,
+    }
+    best_val_loss = run(hparams, experiment_name=None, seed=42)
+    print(best_val_loss)
